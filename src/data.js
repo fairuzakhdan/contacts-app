f29f869e3db8384f69a3e675d3ad266aeaac7f48
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-10-31 13:39:32 +0700

    add component in contacts app

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 const getData = () => {
-    return [
+     return [
       {
         id: 1,
         name: "Dimas Saputra",
         tag: "dimasmds",
-        imageUrl: "public/assets/dimasmds.jpeg",
+        url: "public/assets/images/dimasmds.jpeg",
       },
       {
         id: 2,
         name: "Arif Faizin",
         tag: "arifaizin",
-        imageUrl: "public/assets/arifaizin.jpeg",
+        url: "public/assets/images/arifaizin.jpeg",
       },
       {
         id: 3,
         name: "Rahmat Fajri",
         tag: "rfajri27",
-        imageUrl: "public/assets/rfajri27.jpeg",
+        url: "public/assets/images/rfajri27.jpeg",
       }
     ];
   };
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import ContactApp from "./ContactApp";
 
 
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Result />);
+root.render(<ContactApp />);
