2e5a8a316e480b66e24cf834922fba02b7c4fbc7
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-10-31 12:20:54 +0700

    add getData contacts

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: "Dimas Saputra",
+        tag: "dimasmds",
+        imageUrl: "public/assets/dimasmds.jpeg",
+      },
+      {
+        id: 2,
+        name: "Arif Faizin",
+        tag: "arifaizin",
+        imageUrl: "public/assets/arifaizin.jpeg",
+      },
+      {
+        id: 3,
+        name: "Rahmat Fajri",
+        tag: "rfajri27",
+        imageUrl: "public/assets/rfajri27.jpeg",
+      }
+    ];
+  };
+
+  export {getData};
